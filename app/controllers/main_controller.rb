d68877ed270ee7d5589f662f16c8f7cd7fa41750
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2015-02-21 08:13:56 -0500

    Finishing up landing page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,4 +3,7 @@ class MainController < ApplicationController
 	def main
 	end
 
+	def select
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
   root 'main#index'
+  root 'main#select', as: :select
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
